c9a4515223fa75ff2882e67da26cb9eebf0174d4
Ward Slager <ward.slager@student.hku.nl>
2017-09-12 11:29:19 +0200

    Added short-term memory

--- CSD2a/helloWorld2.py
+++ CSD2a/helloWorld2.py
@@ -1,7 +1,15 @@
 import time
 print("Hey, my name is Forgetto Bottini. What is your name?")
+name = input("-> ")
+lastName = name
 while True :
-    naam = input("-> ")
-    print("Hello", naam)
+    if name == lastName:
+        print("Hello", name)
+    else:
+        print("I thought it was", lastName)
+        time.sleep(1)
+        print("I'll try to remember your name,", name)
     time.sleep(2)
     print("I seem to have forgotten your name, could you please tell me again?")
+    lastName = name
+    name = input("-> ")
