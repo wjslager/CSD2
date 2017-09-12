6a57d83b32267969a628a22b282b0d08835ef132
Ward Slager <ward.slager@student.hku.nl>
2017-09-12 08:56:52 +0200

    Added helloWorld2.py file

---
+++ CSD2a/helloWorld2.py
@@ -0,0 +1,7 @@
+import time
+print("Hey, my name is Forgetto Bottini. What is your name?")
+while True :
+    naam = input("-> ")
+    print("Hello", naam)
+    time.sleep(2)
+    print("I seem to have forgotten your name, could you please tell me again?")
