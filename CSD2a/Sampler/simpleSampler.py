0297a4b40038a56431db2240d7cc040786e3d92a
Ward Slager <ward.slager@student.hku.nl>
2017-09-27 12:43:21 +0200

    Added BPM input with a loop verifying the input

--- CSD2a/Sampler/simpleSampler.py
+++ CSD2a/Sampler/simpleSampler.py
@@ -1,4 +1,9 @@
-#TODO play a sound
+# TODO
+# - Option to input each list at booting the program
+# - Make a sample playing class allowing more sounds to be used without copy-pasting code
+
+# ISSUES
+# - Samples are cutoff when a new sample is triggered
 
 import simpleaudio as sa
 import time
@@ -7,29 +12,41 @@ kickWav = sa.WaveObject.from_wave_file("kick.wav")
 snareWav = sa.WaveObject.from_wave_file("snare.wav")
 hhWav = sa.WaveObject.from_wave_file("hh.wav")
 
+# Lists with drumtriggers
 kickTrig =  [1, 0, 0, 0, 1, 1, 0, 0]
 snareTrig = [0, 0, 1, 0, 0, 0, 0, 1]
 hhTrig =    [1, 1, 1, 1, 1, 1, 1, 1]
 
 trigCount = 0
-bpm = 120
+trigsPerBeat = 2
+
+# Ask for input and check if input is usable as BPM
+validBPM = False
+inputBPM = input("Enter BPM: ")
+
+while validBPM == False:
+    if inputBPM.isdigit():
+        # Set BPM, exit loop, start playback
+        bpm = int(inputBPM)
+        validBPM = True
+        playback = True
+    else:
+        # Ask for input
+        print(" BPM must be an integer.")
+        inputBPM = input("Enter BPM: ")
 
 print("BPM is", bpm)
-while True :
-    print("trigPos is", trigCount%8)
 
+# Check lists for each sound
+while playback == True :
     if kickTrig[trigCount%8] == 1:
-        play_obj = kickWav.play()
+        playObj = kickWav.play()
 
     if snareTrig[trigCount%8] == 1:
-        play_obj = snareWav.play()
+        playObj = snareWav.play()
 
-    time.sleep(0.01) 
     if hhTrig[trigCount%8] == 1:
-        play_obj = hhWav.play()
-        
-    time.sleep(0.2)
+        playObj = hhWav.play()
+       
+    time.sleep(60/bpm/trigsPerBeat)
     trigCount += 1
-
-# play_obj.wait_done()
-# heb ik dit nodig??
