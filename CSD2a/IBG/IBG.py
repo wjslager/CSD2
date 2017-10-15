008fa30b38b43861cdd7414941de5b3279f1dbf1
Ward Slager <ward.slager@student.hku.nl>
2017-10-15 14:00:39 +0200

    Replaced duplicate code with a single function

--- CSD2a/IBG/IBG.py
+++ CSD2a/IBG/IBG.py
@@ -1,17 +1,17 @@
 # CHECKLIST
-# - Option to input each list at booting the program
+# - Option to input each list at booting the program (or load from disk)
 # - Add variation over time: list manipulation
-#       if interval has passed else wait a ~10ms
-# - check all files ending with *.wav -> laad deze
+# - Check all files ending with *.wav and load them
 
 # ISSUES
 # - Clicks when samples are retriggered
 # - Timing inbetween triggers is sloppy
 
-
 import simpleaudio as sa
 import time
 import random
+
+# = == === ===== ======  # Classes and functions # ====== ===== ==== === == = #
 
 # Class which plays a single sample
 class samplePlayer:
@@ -34,35 +34,50 @@ class samplePlayer:
 
 # Functions which initializes all variables needed for playback
 def initPlayback(bpm):
-    global playbackStart, trigCount, triggerLength
+    global trigCount, trigsPerBeat, triggerLength, playbackStart, playback
     
     trigCount = 0
     trigsPerBeat = 4
     triggerLength = 60/bpm/trigsPerBeat
-    print(" Initializing playback \n - A single trigger takes", int(1000*triggerLength), "ms\n -", trigsPerBeat, "triggers per quarter note")
+    print("Initializing playback \n- A single trigger takes", int(1000*triggerLength), "ms\n-", trigsPerBeat, "triggers per quarter note")
 
     playbackStart = time.time()
+    playback = True
+
+# Functions which validates input values
+def checkInput(low, high):
+    global checkedInput
+    validInput = False
+    inputValue = input("> ")
+    
+    while validInput == False:
+        if inputValue.isdigit() and int(inputValue) >= low and int(inputValue) <= high:
+            # Store value and exit loop
+            checkedInput = int(inputValue)
+            validInput = True
+        else:
+            # Try again
+            print("Invalid input. Must be an integer ranging from", low, "to", high)
+            inputValue = input("> ")
+
+# = == === ==== ===== ====== # Playback preparations # ====== ===== ==== === == = #
+
+# Ask for drumkit selection
+# Load corresponding samples
+# Initialize a sample player for each sample
+# Define base patterns
+# Ask for BPM input
+# Initialize playback
 
 # Drumkit selection
-validKit = False
-print("1: Synthetic \n2: n/a \n3: n/a")
-inputKit = input("Choose a drumkit: ")
-
-# Check if drumkit input is valid and try again if needed
-while validKit == False:
-    if inputKit.isdigit() and int(inputKit) < 4:
-        # Set Kit, exit loop
-        drumkit = int(inputKit)
-        validKit = True
-    else:
-        # Try again
-        print(" Drumkit must be an integer smaller than 5")
-        inputKit = input("Choose a drumkit: ")
+print("Available drumkits: \n 0: Synthetic \nChoose a drumkit: ")
+checkInput(0, 0)
+drumkit = checkedInput
 
 # Load the chosen drumkit
-sample0 = sa.WaveObject.from_wave_file("kik" + str(drumkit-1) + ".wav")
-sample1 = sa.WaveObject.from_wave_file("snr" + str(drumkit-1) + ".wav")
-sample2 = sa.WaveObject.from_wave_file("hhc" + str(drumkit-1) + ".wav")
+sample0 = sa.WaveObject.from_wave_file("kik" + str(drumkit).zfill(3) + ".wav")
+sample1 = sa.WaveObject.from_wave_file("snr" + str(drumkit).zfill(3) + ".wav")
+sample2 = sa.WaveObject.from_wave_file("hhc" + str(drumkit).zfill(3) + ".wav")
 
 samples = [sample0, sample1, sample2]
 
@@ -72,38 +87,29 @@ for i in range(len(samples)):
     sounds.append(samplePlayer(i))
 
 # Sequence of drumtriggers
+# 0 no trigger
+# 1 normal trigger, plays a sound
+# 2 random trigger, sometimes plays a sound
 seq0 = [1, 0, 0, 0, 1, 0, 0, 0]
 seq1 = [0, 0, 0, 0, 1, 0, 0, 0]
-seq2 = [0, 2, 1, 2, 0, 2, 1, 2]
+seq2 = [0, 1, 1, 1, 0, 1, 1, 1]
 
 sequences = [seq0, seq1, seq2]
 
-# Set the BPM
-validBPM = False
-inputBPM = input("Choose a BPM: ")
+print("Choose a BPM: ")
+checkInput(50, 200)
+bpm = checkedInput
 
-# Check if BPM input is valid and try again if needed
-while validBPM == False:
-    if inputBPM.isdigit():
-        # Set BPM, exit loop, start playback
-        bpm = int(inputBPM)
-        validBPM = True
-        playback = True
-    else:
-        # Try again
-        print(" BPM must be an integer.")
-        inputBPM = input("Choose a BPM: ")
-
-
-initPlayback(bpm)
+# = == === ==== ===== ====== # Playback Loop # ====== ===== ==== === == = #
 
 # Save time used for checking timing inbetween notes
 timing = time.time()
 
-while True:
-    
-    while playback == True:
+# Calculates the length of triggers
+initPlayback(bpm)
 
+while True:
+    while playback == True:
         # Calculates at which exact time the next event should play
         nextTriggerTime = playbackStart + trigCount * triggerLength
 
