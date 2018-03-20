0dbf7df95f887dfddab9f29e7dd5849e728bf265
Ward Slager <ward.slager@student.hku.nl>
2018-03-20 19:56:47 +0100

    First version, input and output works

---
+++ CSD2c/assignments/pieffect/main.cpp
@@ -0,0 +1,96 @@
+/**********************************************************************
+*                     Copyright (c) 2018, Ward Slager
+*                        Utrecht, the Netherlands
+*                          All rights reserved
+***********************************************************************
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program.
+*  If not, see <http://www.gnu.org/licenses/>.
+***********************************************************************
+*
+*  File name     : main.cpp
+*  System name   : main
+*
+*  Description   : Audio effect (delay/reverb)
+*
+*  Author        : Ward Slager
+*  E-mail        : ward.slager@student.hku.nl
+*
+**********************************************************************/
+
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <thread>
+#include "jack_module.h"
+
+JackModule jack;
+unsigned long samplerate = 48000; // default, overwritten by Jack server
+unsigned long buffersize = 128;
+
+// ===== ===== ===== ===== ===== audio thread ===== ===== ===== ===== ===== //
+
+static void audio()
+{
+  float *inbuffer = new float[buffersize];
+  float *outbuffer = new float[buffersize];
+
+  // The JackModule has created a ringbuffer for both the input and output
+  // This will allow us to use our own buffer size instead of the Jack buffer size
+  while (true)
+  {
+    jack.readSamples(inbuffer, buffersize);
+
+    for (unsigned int n = 0; n < buffersize; n++)
+    {
+      outbuffer[n] = inbuffer[n] * 0.1;
+    }
+
+    jack.writeSamples(outbuffer, buffersize);
+  }
+}
+
+// ===== ===== ===== ===== ===== main() ===== ===== ===== ===== ===== //
+
+int main(int argc, char **argv)
+{
+  // Start Jack and retrieve the samplerate
+  jack.init("pieffect");
+  jack.autoConnect();
+  samplerate = jack.getSamplerate();
+
+  std::cerr << "\nSamplerate\t" << samplerate << "\nBuffer size\t" << buffersize << std::endl;
+
+  // Start the DSP thread
+  std::thread dspThread(audio);
+
+  // Wait for commandline input
+  while(true)
+  {
+    std::string input;
+    std::cin >> input;
+
+    if (input == "q" || input == "e" || input == "quit" || input == "exit") {
+      exit(0);
+    }
+    else {
+      std::cerr << "Command not recognized: " << input << std::endl;
+    }
+  }
+
+  dspThread.join();
+
+  // Disconnect Jack and end the program
+  jack.end();
+  return 0;
+}
